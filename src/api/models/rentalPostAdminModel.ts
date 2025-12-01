5fcc54d6bf0cbd2cacf8f640c2f9f0ac208a7169
q-dat <quocdatstore79@gmail.com>
2025-12-01 20:40:04 +0700

    feat: update rentalPostAdminAPI

--- src/api/models/rentalPostAdminModel.ts
+++ src/api/models/rentalPostAdminModel.ts
@@ -13,11 +13,21 @@ export interface IRentalPostAdmin extends Document {
   title: string // tiêu đề bài đăng
   description: string // mô tả chi tiết nội dung bài đăng
   category: IRentalCategory // danh mục (liên kết đến bảng danh mục)
-  price: number // giá cho thuê (VNĐ)
-  priceUnit: string // đơn vị giá (vd: "VNĐ/tháng")
+  propertyType?: string // loại hình bất động sản (vd: "Căn hộ", "Nhà phố",...)
+  locationType?: string // loại hình vị trí (vd: "đường lớn", "hẻm",...)
+  direction?: string // hướng nhà (vd: "Đông", "Tây",...)
+  price: number // Giá bán hoặc giá thuê
+  priceUnit: string // VD: "Tỷ", "Triệu/m²", "/tháng", "/m²"...
+  pricePerM2?: number // giá theo m2 (VNĐ/m2)
   area: number // diện tích (m2)
-  length?: string // chiều dài
+  length?: string // chiều ngang
   width?: string // chiều rộng
+  backSize?: string // mặt hậu
+  floorNumber?: number // số tầng
+  bedroomNumber?: number // số phòng ngủ
+  toiletNumber?: number // số toilet
+  legalStatus?: string // tình trạng pháp lý (vd: "sổ hồng", "sổ đỏ",...)
+  furnitureStatus?: string // tình trạng nội thất (vd: "đầy đủ", "chưa có",...)
   province: string // tỉnh/thành phố
   district: string // quận/huyện
   ward?: string // phường/xã
@@ -45,19 +55,35 @@ const RentalPostSchema: Schema = new Schema(
     title: { type: String, required: true, trim: true },
     description: { type: String, required: true, trim: true },
     category: { type: Schema.Types.ObjectId, ref: 'RentalCategoryModel', required: true },
+
+    propertyType: { type: String },
+    locationType: { type: String },
+    direction: { type: String },
+
     price: { type: Number, required: true, min: 0 },
     priceUnit: { type: String, required: true },
+    pricePerM2: { type: Number, min: 0 },
+
     area: { type: Number, required: true, min: 0 },
     length: { type: String },
     width: { type: String },
+    backSize: { type: String },
+    floorNumber: { type: Number, min: 0 },
+    bedroomNumber: { type: Number, min: 0 },
+    toiletNumber: { type: Number, min: 0 },
+    legalStatus: { type: String },
+    furnitureStatus: { type: String },
+
     province: { type: String, required: true },
     district: { type: String, required: true },
     ward: { type: String },
     address: { type: String, required: true },
+
     amenities: { type: String },
     youtubeLink: { type: String },
     videoTitle: { type: String },
     videoDescription: { type: String },
+
     postType: {
       type: String,
       enum: ['basic', 'vip1', 'vip2', 'vip3', 'highlight'],
