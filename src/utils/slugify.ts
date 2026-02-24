62b2a33222e1a06c7550744a30a0ea0204fb478f
q-dat <quocdatstore79@gmail.com>
2026-02-24 19:55:59 +0700

    feat: add slugify config

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,15 +1,19 @@
-import slugifyLib from 'slugify';
+import slugifyLib from 'slugify'
 
 export const slugify = (text: string): string => {
-  const normalized = text.replace(/\//g, '-');
+  const normalized = text
+    .replace(/[\/|_]/g, '-') // thay các ký tự phân cách phổ biến
+    .replace(/\s+/g, ' ') // normalize space
+    .normalize('NFD') // tách ký tự có dấu thành ký tự thường + dấu
+    .replace(/[\u0300-\u036f]/g, '') // loại bỏ dấu
 
   return slugifyLib(normalized, {
     lower: true,
     strict: true,
     trim: true,
     locale: 'vi'
-  });
-};
+  })
+}
 
 // const test = slugify('123 test/slugify');
 // alert(test);
